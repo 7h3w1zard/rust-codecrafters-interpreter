3b613e82014d8fca5a95eb141c230802fabb35cf
7h3w1zard <93vrs93@gmail.com>
2025-11-22 22:30:33 +0300

    Scanning: Parentheses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 
+pub(crate) mod token;
+pub(crate) mod scanner;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -14,13 +17,15 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
+            // writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                scanner::Scanner::scan_tokens(file_contents);
             } else {
                 println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
             }
---
+++ src/scanner.rs
@@ -0,0 +1,21 @@
+use crate::token::TokenType;
+
+pub(crate) struct Scanner {
+    source: String,
+}
+
+impl Scanner {
+    pub(crate) fn scan_tokens(source: String) {
+        let null = "null";
+        
+        for c in source.chars() {
+            match c {
+                '(' => println!("{:?} {c} {null}", TokenType::LEFT_PAREN),
+                ')' => println!("{:?} {c} {null}", TokenType::RIGHT_PAREN),
+                _ => panic!("Unexpected character."),
+            }
+        }
+
+        println!("EOF  null");
+    }
+}
